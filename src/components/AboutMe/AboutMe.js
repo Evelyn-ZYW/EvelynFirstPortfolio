11f885ce770e014a2edaf1c7051c9a3ad06d96b4
Evelyn Z <evelynzdragon@gmail.com>
2021-11-01 20:32:27 -0700

    fixed responsiveness bug

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -26,7 +26,7 @@ const AboutMe = () => {
         <div className="subtitle">
           {/* <h3>Front End Developer</h3> */}
           <p>
-            Versatile Front End Developer with a keen eye to detail. Experienced
+            Enthusiastic Front End Developer with a keen eye to detail. Experienced
             working with teams to produce impactful, leading-edge web/mobile
             applications that engage customers and deliver business results.
             Well-versed in design standards and user preferences.
@@ -34,7 +34,7 @@ const AboutMe = () => {
         </div>
       </div>
       {display? 
-          <lottie-player src="https://assets6.lottiefiles.com/datafiles/TECfNlBjjBlUhNg/data.json"  background="transparent" speed="1" loop autoplay mode="normal" id="scroll-down"></lottie-player> : null }
+          <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_jfchliut.json"  background="transparent" speed="1" loop autoplay mode="normal" id="scroll-down"></lottie-player> : null }
 
     </Container>
   );
